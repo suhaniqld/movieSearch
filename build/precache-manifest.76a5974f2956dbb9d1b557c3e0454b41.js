4b40b279d838110fc6bd27f18488d8ab407c2979
Suhani Varshney <suhani.qld@gmail.com>
2019-09-23 08:05:39 +1000

    Adding some styles

---
+++ build/precache-manifest.76a5974f2956dbb9d1b557c3e0454b41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8cb201741e5cd15f482e",
+    "url": "/static/js/main.8cb20174.chunk.js"
+  },
+  {
+    "revision": "a1032644f416deb945a9",
+    "url": "/static/js/1.a1032644.chunk.js"
+  },
+  {
+    "revision": "8cb201741e5cd15f482e",
+    "url": "/static/css/main.73e3cb88.chunk.css"
+  },
+  {
+    "revision": "1132e6a6e199452c8e09ef4eb86e8a76",
+    "url": "/index.html"
+  }
+];
